8de0cd6ea575fc9703429d2ab96ffe63f90829a4
mckinleysouder <msouder@ucsd.edu>
2021-05-26 14:34:20 -0700

    Data is processed for model training

--- server/data/data.py
+++ server/data/data.py
@@ -6,6 +6,8 @@ from firebase_admin import credentials, db, initialize_app, firestore
 import pandas as pd
 import sklearn # import scikit-learn
 from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
 
 cred = credentials.Certificate('key.json')
 default_app = initialize_app(cred)
@@ -14,33 +16,50 @@ default_app = initialize_app(cred)
 db = firestore.client()
 docs = db.collection('game_played').where('dead_or_alive', '!=', -1).stream()
 
-#converting documents to a dataframe for training
+# #chronological progression of choices
+# featureIdx = {
+#   "airport_arrival": 0,     #DTF or Starbucks
+#   "starbucks": 1,           #Regular coffee or 3 shots
+#   "board_flight": 2,        #sleep or watch her
+#   "watch_her": 3,           #keep watching or get coffee
+#   "get_coffee": 4,          #walk into cockpit or return to seat
+#   "walk_into_cockpit": 5,   #help captain or help first officer
+#   "return_seat": 6,         #call attendant or settle in seat
+# }
+
+#converting documents to a dataframe to hold database training data
 full_df = pd.DataFrame()
 for doc in docs:
     whatType = doc.to_dict()
     full_df = full_df.append(doc.to_dict(), ignore_index=True)
 
+features_and_label = ['airport_arrival', 'starbucks', 'board_flight', 'watch_her', 'get_coffee', 'walk_into_cockpit', 'return_seat', 'dead_or_alive']#decide if we need label here
 
-#chosen features decides which fork this model represents
-chosen_features = ['starbucks', 'get_coffee']
+chosen_features = ['airport_arrival', 'starbucks', 'board_flight', 'watch_her', 'get_coffee', 'walk_into_cockpit', 'return_seat']
 
-chosen_df = full_df[chosen_features]
-print(chosen_df.head())
+#take only needed features and label to predict
+chosen_df = full_df[features_and_label]
 
-enc = preprocessing.OneHotEncoder()
-enc.fit(chosen_df) # fit the encoder to categories in our data 
-one_hot = enc.transform(chosen_df) # transform data into one hot encoded sparse array format
-# Finally, put the newly encoded sparse array back into a pandas dataframe so that we can use it
 
-chosen_df_proc = pd.DataFrame(one_hot.toarray(), columns=enc.get_feature_names())
-print(chosen_df_proc.head())
+#Now we need to break up each row into a vector for each choice
+expanded_df = pd.DataFrame()
 
-labels = full_df['dead_or_alive']
+for playthroughIdx in range(len(chosen_df)):
+    playthrough = chosen_df.loc[playthroughIdx]
+    for choice in range(len(playthrough)-2,0,-1):
+        if (playthrough[choice] != -1):
+            playthrough[choice] = -1
+            expanded_df = expanded_df.append(playthrough, ignore_index=True)
 
-#maybe fill NaN here
 
-#building the decision tree
-from sklearn.tree import DecisionTreeClassifier
+#split processed data into features and labels
+features = expanded_df[chosen_features]
+labels = expanded_df['dead_or_alive']
 
-model = DecisionTreeClassifier().fit(chosen_df_proc, labels) # first fit (train) the model
-#hint = model.predict(currentRunData)
+#training the decision tree
+model = DecisionTreeClassifier().fit(features, labels)
+
+#testing predictions
+array = np.array([1, 0, 1, 1, 1, -1, -1])
+hint = model.predict(array.reshape(1, -1))
+print(hint)
