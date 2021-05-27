d5acd67b083598bb90c2adce6abd35b11e46a363
mckinleysouder <msouder@ucsd.edu>
2021-05-27 13:12:52 -0700

    Test/Validation set, metrics, and first try at probability of a win added

--- server/data/data.py
+++ server/data/data.py
@@ -7,7 +7,19 @@ import pandas as pd
 import sklearn # import scikit-learn
 from sklearn import preprocessing
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 import numpy as np
+
+def metrics(y_true, y_pred):
+    print('Confusion matrix:\n', confusion_matrix(y_true, y_pred))
+    print('\nReport:\n', classification_report(y_true, y_pred))
+
+def probabilityOfAWin(currentPlaythrough, data):
+    numWins = len(data[(data['airport_arrival'] == currentPlaythrough[0]) & (data['starbucks'] == currentPlaythrough[1]) & (data['board_flight'] == currentPlaythrough[2]) & (data['watch_her'] == currentPlaythrough[3]) & (data['get_coffee'] == currentPlaythrough[4]) & (data['walk_into_cockpit'] == currentPlaythrough[5]) & (data['return_seat'] == currentPlaythrough[6]) & (data['dead_or_alive'] == 1)])
+    return numWins/(len(data))
+
 
 cred = credentials.Certificate('key.json')
 default_app = initialize_app(cred)
@@ -52,14 +64,26 @@ for playthroughIdx in range(len(chosen_df)):
             expanded_df = expanded_df.append(playthrough, ignore_index=True)
 
 
+
 #split processed data into features and labels
 features = expanded_df[chosen_features]
 labels = expanded_df['dead_or_alive']
 
+
+#getting percentage of win possibility from current playthrough
+X_train, X_TEMP, y_train, y_TEMP = train_test_split(features, labels, test_size=0.30) 
+X_validation, X_test, y_validation, y_test = train_test_split(X_TEMP, y_TEMP, test_size=0.50) 
+print(X_train.shape, X_validation.shape, X_test.shape)
+
 #training the decision tree
-model = DecisionTreeClassifier().fit(features, labels)
+model = DecisionTreeClassifier().fit(X_train, y_train)
 
 #testing predictions
-array = np.array([1, 0, 1, 1, 1, -1, -1])
-hint = model.predict(array.reshape(1, -1))
-print(hint)
+#array = np.array([1, 0, 1, 1, 0, 1, -1])
+#hint = model.predict(array.reshape(1, -1))
+#print(hint)
+
+y_pred = model.predict(X_validation) 
+metrics(y_validation, y_pred) 
+
+print(probabilityOfAWin(np.array([1, 0, 1, 1, 0, 1, -1]), expanded_df))
